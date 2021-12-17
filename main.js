c06a99d067c3bd79bda9a5df7eead8f999047e0a
Sheelove Dol <siesel26@gmail.com>
2021-12-17 10:03:25 -0800

    first part of error handling

---
+++ main.js
@@ -0,0 +1,7 @@
+// Error handling in javascript
+
+console.log(Error('This will print this custom error'))
+console.log('hello')
+
+throw Error('This error is "THROWN" stops any code that comes after it from running');
+
---
+++ try...catch.js
@@ -0,0 +1,2 @@
+// In Javascript we use 'try...catch' statement to anticipate and handle errors
+

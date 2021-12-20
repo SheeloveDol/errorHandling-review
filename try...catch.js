ef1f0e2722b63149e123a4fa19019b412d9b50d4
Sheelove Dol <siesel26@gmail.com>
2021-12-20 09:10:55 -0800

    Real example of try...catch

--- try...catch.js
+++ try...catch.js
@@ -5,3 +5,17 @@ try {
 } catch (e) {
   console.log(e)
 }
+
+
+
+// another real life example of try...catch
+
+function capAllElements(arr){
+	try {arr.forEach((el, index, array) => {
+    array[index] = el.toUpperCase();
+  });} catch(e) {
+    console.log(e)
+  }
+}
+
+capAllElements('Incorrect argument');

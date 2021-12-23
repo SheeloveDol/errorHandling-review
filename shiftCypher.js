ab448e629bdb678cb99ac45a2b6a72d392509404
Sheelove Dol <siesel26@gmail.com>
2021-12-22 20:03:21 -0800

    elegant solution

--- shiftCypher.js
+++ shiftCypher.js
@@ -9,19 +9,71 @@
   
 // }
 
-const str = 'hello world';
-const array = str.split('')
-// console.log(array.join(''))
+// const str = 'hezzlloworld';
+// const array = str.split('')
+// // console.log(array.join(''))
 
-const alphabet = 'abcdefghijklmopqrstuvwxyz'.split('')
+// const alphabet = 'abcdefghijklmopqrstuvwxyz'.split('')
 
-console.log()
+// console.log()
 
- array.forEach(l => {
-  console.log(alphabet[alphabet.indexOf(l) + 4].toUpperCase())
-  })
+//  array.forEach(l => {
+//   console.log(alphabet[alphabet.indexOf(l) + 4].toUpperCase())
+//   })
 
-console.log(alphabet)
+// console.log(alphabet)
 
-let shift = 4
-//  console.log(messing)
+// let shift = 4
+// //  console.log(messing)
+
+
+
+
+//SOLUTION//
+// Write class below
+class ShiftCipher {
+  constructor(shift){
+    this.shift = shift;
+  }
+  encrypt(plainString) {
+    let encryptString = '';
+    const tempString = plainString.toUpperCase();
+
+    for (let i=0; i < tempString.length; i++) {
+      let charNum = tempString.charCodeAt(i);
+      
+      if (charNum <= 90 && charNum >= 65) {
+        charNum += this.shift;
+        if (charNum > 90) {
+          charNum -= 26;
+        }
+      }
+      encryptString += String.fromCharCode(charNum);
+    }
+    return encryptString;
+  }
+
+  decrypt(encryptString) {
+    let decryptString = '';
+    const tempString = encryptString.toLowerCase();
+
+    for (let i=0; i < tempString.length; i++) {
+      let charNum = tempString.charCodeAt(i);
+      
+      if (charNum <= 122 && charNum >= 97) {
+        charNum -= this.shift;
+        if (charNum < 97) {
+          charNum += 26;
+        }
+      }
+      decryptString += String.fromCharCode(charNum);
+    }
+    return decryptString;
+  }
+}
+
+const word = 'abcdefghijklmnopqrstuvwxyz'
+
+for (let i = 0; i < word.length; i++) {
+  console.log(word.charCodeAt(i))
+}
